6d9aa892c8edc00a6054e6dea6f95770c2380734
Luismkm <luismarcelo13@yahoo.com.br>
2021-12-13 22:05:39 -0300

    feat: add interface createUserDTO type

--- src/modules/users/infra/typeorm/repositories/UsersRerpositories.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRerpositories.ts
@@ -1,7 +1,7 @@
 import { getRepository, Repository } from 'typeorm';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import ICreateClientDTO from '@modules/clients/dtos/ICreateClientDTO';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import User from '../entities/User';
 
@@ -12,7 +12,7 @@ class UsersRepository implements IUsersRepository {
     this.ormRepository = getRepository(User);
   }
 
-  public async create({ name, password, role }: any): Promise<User> {
+  public async create({ name, password, role }: ICreateUserDTO): Promise<User> {
     const userData = this.ormRepository.create({ name, password, role });
 
     await this.ormRepository.save(userData);
