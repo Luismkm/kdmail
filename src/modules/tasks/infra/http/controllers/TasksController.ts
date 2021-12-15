07baf21945643be3205009cda9262430cb115407
Luismkm <luismarcelo13@yahoo.com.br>
2021-12-15 20:52:59 -0300

    refactor: pega o id do usuario da request

--- src/modules/tasks/infra/http/controllers/TasksController.ts
+++ src/modules/tasks/infra/http/controllers/TasksController.ts
@@ -5,8 +5,7 @@ import CreateTaskServices from '@modules/tasks/services/CreateTaskServices';
 
 export default class TasksController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // const user_id = request.user.id;
-    const user_id = '6775d21d-9ba0-4036-b636-d09a8450e132';
+    const user_id = request.user.id;
     const { description, status } = request.body;
 
     const createTask = container.resolve(CreateTaskServices);
