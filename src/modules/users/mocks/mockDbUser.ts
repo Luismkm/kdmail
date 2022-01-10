c8f22defab219d4f996d2491cb0166a5a7c28b39
Luismkm <luismarcelo13@yahoo.com.br>
2022-01-10 19:52:54 -0300

    test: add mockCreateUserRepository

--- src/modules/users/mocks/mockDbUser.ts
+++ src/modules/users/mocks/mockDbUser.ts
@@ -1,4 +1,6 @@
+import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
 import { User } from '../infra/typeorm/entities/User';
+import { ICreateUserRepository } from '../repositories/ICreateUserRepository';
 import { ILoadUserByNameRepository } from '../repositories/ILoadUserByNameRepository';
 
 export const mockLoadUserByNameRepository = (): ILoadUserByNameRepository => {
@@ -14,4 +16,19 @@ export const mockLoadUserByNameRepository = (): ILoadUserByNameRepository => {
     }
   }
   return new LoadUserByNameRepositoryStub();
+};
+
+export const mockCreateUserRepository = (): ICreateUserRepository => {
+  class CreateUserRepositoryStub implements ICreateUserRepository {
+    async create(data: ICreateUserDTO): Promise<User> {
+      return Promise.resolve({
+        id: 'any_id',
+        name: 'any_name',
+        password: 'hashed_password',
+        role: 'any_role',
+        created_at: new Date('11/11/2020'),
+      });
+    }
+  }
+  return new CreateUserRepositoryStub();
 };
