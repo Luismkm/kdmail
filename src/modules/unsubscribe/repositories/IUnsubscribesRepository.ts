af502d1c8476e3df5a09ce8c2a23c06c2eb2a512
Luismkm <luismarcelo13@yahoo.com.br>
2022-01-16 19:50:00 -0300

    refactor: renomeia arquivo

--- src/modules/unsubscribe/repositories/IUnsubscribesRepository.ts
+++
@@ -1,6 +0,0 @@
-import Unsubscribe from '../infra/typeorm/entities/Unsubscribe';
-
-export default interface IUnsubscribesRepository {
-  create(cod: string, email: string): Promise<void>;
-  findAllUnsubscribe(): Promise<Unsubscribe[]>;
-}
