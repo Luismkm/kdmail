beb52bb2a708ea004d1ec796ee79f753321af459
Luismkm <luismarcelo13@yahoo.com.br>
2022-01-16 19:36:15 -0300

    refactor: atualiza import

--- src/modules/users/providers/index.ts
+++ src/modules/users/providers/index.ts
@@ -1,6 +1,9 @@
 import { container } from 'tsyringe';
 
-import IHashProvider from './HashProvider/models/IHashProvider';
 import BCriptHashProvider from './HashProvider/implementations/BCriptHashProvider';
+import { IHashProviderInject } from './HashProvider/models/IHashProviderInject';
 
-container.registerSingleton<IHashProvider>('HashProvider', BCriptHashProvider);
+container.registerSingleton<IHashProviderInject>(
+  'HashProvider',
+  BCriptHashProvider,
+);
