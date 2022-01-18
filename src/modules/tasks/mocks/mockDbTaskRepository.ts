fb99a39fa617647ba9da600f81659cbd465e3cf8
Luismkm <luismarcelo13@yahoo.com.br>
2022-01-18 20:57:07 -0300

    test: add mockFindAllOpenTaskRepository

--- src/modules/tasks/mocks/mockDbTaskRepository.ts
+++ src/modules/tasks/mocks/mockDbTaskRepository.ts
@@ -1,6 +1,10 @@
 import ICreateTaskDTO from '../dtos/ICreateTaskDTO';
 import Tasks from '../infra/typeorm/entities/Task';
-import { ICreateTaskRepository, IDeleteTaskRepository } from '../repositories';
+import {
+  ICreateTaskRepository,
+  IDeleteTaskRepository,
+  IFindAllOpenTaskRepository,
+} from '../repositories';
 
 export const mockCreateTaskRepository = (): ICreateTaskRepository => {
   class CreateTaskRepositoryStub implements ICreateTaskRepository {
@@ -23,4 +27,30 @@ export const mockDeleteTaskRepository = (): IDeleteTaskRepository => {
     async delete(task_id: string): Promise<void> {}
   }
   return new DeleteTaskServicesStub();
+};
+
+export const mockFindAllOpenTaskRepository = (): IFindAllOpenTaskRepository => {
+  class FindAllOpenTaskRepositoryStub implements IFindAllOpenTaskRepository {
+    async findAllOpenTask(): Promise<Tasks[]> {
+      return Promise.resolve([
+        {
+          id: 'any_id',
+          user_id: 'any_user_id',
+          description: 'any_description',
+          status: 'any_status',
+          created_at: new Date('11/11/2020'),
+          updated_at: new Date('11/11/2020'),
+        } as Tasks,
+        {
+          id: 'other_id',
+          user_id: 'other_user_id',
+          description: 'other_description',
+          status: 'other_status',
+          created_at: new Date('11/11/2020'),
+          updated_at: new Date('11/11/2020'),
+        } as Tasks,
+      ]);
+    }
+  }
+  return new FindAllOpenTaskRepositoryStub();
 };
